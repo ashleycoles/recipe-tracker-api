798b192eec005f25809278ad84d082c6b7412432
Ashley Coles <ashley@acoles.co.uk>
2020-12-11 16:03:07 +0000

    Removing unused imports

--- schema.js
+++ schema.js
@@ -1,31 +1,12 @@
 const graphql = require('graphql');
 
 const {
-    GraphQLObjectType,
-    GraphQLString,
     GraphQLSchema,
-    GraphQLList,
-    GraphQLNonNull
 } = graphql;
 
 // Mongoose Operations
 const RootQuery = require('./graphQlOperations/RootQuery');
 const Mutation = require('./graphQlOperations/Mutation');
-
-// Mongoose Models
-const RecipeModel = require('./mongoModels/RecipeModel');
-const CuisineModel = require('./mongoModels/CuisineModel');
-
-// GraphQL Types
-const IngredientInputType = require('./graphqlTypes/IngredientInputType');
-const RecipeType = require('./graphqlTypes/RecipeType');
-const CuisineType = require('./graphqlTypes/CuisineType');
-
-
-
-
-
-
 
 module.exports = new GraphQLSchema({
     query: RootQuery,
